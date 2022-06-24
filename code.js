1d7a9ebb89d98c17add5c404c885efc1cd005452
Hannes Egler <hannes@egler.me>
2022-06-24 15:42:24 +0200

    changed pages

--- code.js
+++ code.js
@@ -1,30 +1,27 @@
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-let Refferences = figma.createPage();
-let UXApproaches = figma.createPage();
-let ContentApproaches = figma.createPage();
-let UIApproaches = figma.createPage();
-let FinalUI = figma.createPage();
-let Prototype = figma.createPage();
-let Showcase = figma.createPage();
+let Research = figma.createPage();
+let UXDrafts = figma.createPage();
+let Archive = figma.createPage();
+let UIDrafts = figma.createPage();
+let Handover = figma.createPage();
 let Cover = figma.currentPage;
 let CoverFrame = figma.createFrame();
 let CoverHead = figma.createText();
 let CoverDesc = figma.createText();
-figma.currentPage.name = "⬜️ Cover";
-Refferences.name = "🔭 References";
-UXApproaches.name = "📋 UX Approaches";
-ContentApproaches.name = "✍🏼 Content Approaches";
-UIApproaches.name = "👁 UI Approaches";
-FinalUI.name = "👍 Final UI";
-Prototype.name = "📱 Prototype";
-Showcase.name = "🖥 Showcase";
+figma.currentPage.name = "🖼 Cover";
+Research.name = "🕵️ Research";
+UXDrafts.name = "📋 UX Approaches";
+Archive.name = "      ↳ Archiv";
+UIDrafts.name = "🎨 UI Drafts";
+Handover.name = "✅ Handover";
 CoverFrame.name = "Cover";
 Cover.appendChild(CoverFrame);
 CoverFrame.appendChild(CoverHead);
@@ -34,14 +31,14 @@ let setPosition = (node, spacex, spacey) => { node.relativeTransform = [[1, 0, s
 let xCalculator = (container, element) => { return ((container.width / 2) - (element.width / 2)); };
 let yCalculator = (container, element) => { return ((container.height / 2) - (element.height / 2)); };
 let loadFontHead = (name) => __awaiter(this, void 0, void 0, function* () {
-    yield figma.loadFontAsync({ family: "Roboto", style: "Bold" });
-    CoverHead.fontName = { family: "Roboto", style: "Bold" };
+    yield figma.loadFontAsync({ family: "Open Sans", style: "Bold" });
+    CoverHead.fontName = { family: "Open Sans", style: "Bold" };
     CoverHead.characters = name;
     CoverHead.fontSize = 74;
     CoverHead.textAlignHorizontal = "CENTER";
 });
 let loadFontDesc = (text) => __awaiter(this, void 0, void 0, function* () {
-    yield figma.loadFontAsync({ family: "Roboto", style: "Regular" });
+    yield figma.loadFontAsync({ family: "Open Sans", style: "Regular" });
     CoverDesc.fontSize = 36;
     CoverDesc.characters = text;
     CoverDesc.textAlignHorizontal = "CENTER";
@@ -58,7 +55,7 @@ let layoutText = () => {
 let run = () => __awaiter(this, void 0, void 0, function* () {
     yield loadFontHead("Add Heading");
     yield loadFontDesc("Add Description");
-    figma.notify("Project Scafolding Done 👍");
+    figma.notify("Project Scaffolding Done 👍");
     figma.closePlugin();
 });
 run();
